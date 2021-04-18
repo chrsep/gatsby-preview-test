ff11025eb3242458fcc2aed41d2028d044c2e0ac
christianto <christianto@ematicsolutions.com>
2021-04-18 19:12:40 +0700

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,23 +5,43 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Footer: FC = () => {
   return (
-    // <nav className={"p-5 mx-auto flex items-center bg-gray-300 w-full"}>
-    //   <Link to={"/"}>
-    //     <StaticImage
-    //       src={"../images/BGP-Logo.webp"}
-    //       alt={"logo"}
-    //       width={216}
-    //       formats={["auto", "webp", "avif"]}
-    //     />
-    //   </Link>
-    //   <Navlinks to={"/"} text={"Home"} className={"ml-auto"} />
-    //   <Navlinks to={"/products"} text={"Our Products"} />
-    //   <Navlinks to={"/about"} text={"About Us"} />
-    //   <Navlinks to={"/contact"} text={"Contact Us"} />
-    // </nav>
-          <footer className={"py-5 bg-gray-700 text-center text-white"}>
-          Tailwind is Awesome 😎
-        </footer>
+    <footer className={"flex py-5 bg-gray-700 text-center text-white"}>
+      <div className={"flex-1 p-4"}>
+        <Link to={"/"}>
+          <StaticImage
+            src={"../images/BGP-Logo.webp"}
+            alt={"logo"}
+            width={216}
+            formats={["auto", "webp", "avif"]}
+          />
+        </Link>
+      </div>
+      <div className={"flex-1 text-white p-4 text-left"}>
+        <h1 className={"text-2xl mb-1"}>Hubungi Kami</h1>
+        <p className="text-xs text-left leading-6">
+          Kantor : Grand Puri Niaga, Jl. Puri Kencana Blok K6 No 5J, Kembangan, Jakarta Barat 11610<br></br>
+Gudang : Jl. Ciraden Desa Citapen, Cihampelas, West Bandung, 40562<br></br>
+Telephone: 021-22345893<br></br>
+Email: administrator@berkatgemilangperkasa.co.id
+          </p>
+      </div>
+      <div className={"flex-1 p-4 text-white text-left"}>
+        <label className="block">
+          <span className="text-white text-sm">Full name</span>
+          <input type="text" className="mt-1 block max-w-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+        </label>
+        <label className="block">
+          <span className="text-white text-sm">Phone</span>
+          <input type="text" className="mt-1 block max-w-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+        </label>
+        <button className="mt-2 mb-2 bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Send
+</button>
+        <p className="text-sm text-left font-bold">
+          <span className="text-xs font-light">Powered By :</span> Ant Digital
+          </p>
+      </div>
+    </footer>
   )
 }
 
